1dbf4c1b75a2268f64c05e4cfb0ce2b6281118d6
AngelEdm <79864206+AngelEdm@users.noreply.github.com>
2021-03-28 15:42:53 -0600

    Holi

--- src/Ciudadanos/CI_login.py
+++ src/Ciudadanos/CI_login.py
@@ -10,32 +10,35 @@ def construir_bp_ciudadano(cliente_mongo, Database, SECRET_KEY):
     @ciudadano_bp.route("/ciudadanos/login", methods=["POST"])
     def login_ciudadano():
 
+        datos_entrada={}
         datos_entrada = request.json
         print(type(datos_entrada))
-        print(datos_entrada)
+        print(datos_entrada) 
 
-        registro_ciudadano = ciudadano_tabla.find_one(datos_entrada)
-        if registro_ciudadano is not None:
+        if ("email" in datos_entrada.keys()) and ("password" in datos_entrada.keys()):
+            registro_ciudadano = ciudadano_tabla.find_one(datos_entrada)
+            if registro_ciudadano is not None:
 
-            token = encode_auth_token_ciudadano(registro_ciudadano["email"], SECRET_KEY)
+                token = encode_auth_token_ciudadano(registro_ciudadano["email"], SECRET_KEY)
 
-            respuesta_datos = {"nombres" : registro_ciudadano["nombres"],
-                                "email" : registro_ciudadano["email"]
-                                }
+                respuesta_datos = {"nombres" : registro_ciudadano["nombres"],
+                                    "email" : registro_ciudadano["email"]
+                                    }
 
-            resulting_response = make_response((respuesta_datos, 200, 
-            {
-                'Access-Control-Allow-Origin': '*', 
-                'mimetype':'application/json',
-                'x-access-token': token
-                }
-            ))
+                resulting_response = make_response((respuesta_datos, 200, 
+                {
+                    'Access-Control-Allow-Origin': '*', 
+                    'mimetype':'application/json',
+                    'x-access-token': token
+                    }
+                ))
 
-            return resulting_response
+                return resulting_response
+            else:
+                return make_response({"autenticacion": False}, 400, {
+                    'Access-Control-Allow-Origin': '*', 
+                    'mimetype':'application/json'
+                    })
         else:
-            return make_response({"autenticacion": False}, 400, {
-                'Access-Control-Allow-Origin': '*', 
-                'mimetype':'application/json'
-                })
-
+            return 'Acceso incorrecto'
     return ciudadano_bp
